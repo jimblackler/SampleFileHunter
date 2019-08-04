3888e151c077fe15b342487f992cb7037c12b097
Jim Blackler <jimblackler@google.com>
2019-08-04 18:10:41 +0100

    More filtering, and files copied to output directory.

--- src/net/jimblackler/samplehunter/Main.java
+++ src/net/jimblackler/samplehunter/Main.java
@@ -2,12 +2,15 @@ package net.jimblackler.samplehunter;
 
 import com.google.common.base.Strings;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.AccessDeniedException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -19,7 +22,7 @@ public class Main {
 
   public static void main(String[] args) throws IOException {
     Random random = new Random(1);
-    Map<String, Map<Integer, Set<String>>> filesByMimeType = new HashMap<>();
+    Map<String, Map<Integer, Set<String>>> filesByType = new HashMap<>();
 
     Files.walkFileTree(Paths.get("/"), new SimpleFileVisitor<>() {
       @Override
@@ -28,19 +31,22 @@ public class Main {
 
         if (Strings.isNullOrEmpty(typeGroup)) {
           typeGroup = com.google.common.io.Files.getFileExtension(file.toString());
+          if (typeGroup.length() > 4 || Strings.isNullOrEmpty(typeGroup)) {
+            return FileVisitResult.CONTINUE;
+          }
         }
 
-        if (Strings.isNullOrEmpty(typeGroup)) {
+        if (!filesByType.containsKey(typeGroup)) {
+          filesByType.put(typeGroup, new HashMap<>());
+        }
+        Map<Integer, Set<String>> files = filesByType.get(typeGroup);
+
+        long length = file.toFile().length();
+        if (length == 0) {
           return FileVisitResult.CONTINUE;
         }
-
-        if (!filesByMimeType.containsKey(typeGroup)) {
-          filesByMimeType.put(typeGroup, new HashMap<>());
-        }
-        Map<Integer, Set<String>> files = filesByMimeType.get(typeGroup);
-
-        int sizeGroup = (int) (Math.log(file.toFile().length()) / Math.log(40));
-        if (sizeGroup >= 3) {
+        int sizeGroup = (int) (Math.log(length) / Math.log(40));
+        if (sizeGroup == 0 || sizeGroup >= 4) {
           return FileVisitResult.CONTINUE;
         }
 
@@ -60,13 +66,20 @@ public class Main {
       }
     });
 
-    for (Map<Integer, Set<String>> files : filesByMimeType.values()) {
+    for (Map<Integer, Set<String>> files : filesByType.values()) {
       for (Set<String> f2 : files.values()) {
         int item = random.nextInt(f2.size());
         int idx = 0;
         for (String str : f2) {
           if (idx == item) {
-            System.out.println(str);
+            try {
+              Path source = Paths.get(str);
+              Files.copy(source, Paths.get("/Users/jimblackler/Downloads/samples/" + new File(str).getName()),
+                  StandardCopyOption.REPLACE_EXISTING);
+              System.out.println(str);
+            } catch (AccessDeniedException e) {
+              e.printStackTrace();
+            }
           }
           idx++;
         }
